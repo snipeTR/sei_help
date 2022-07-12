e782a6482ad60b763d850918e7f87cf1ab6b5a36
snipeTR <31975916+snipeTR@users.noreply.github.com>
2022-07-12 15:03:42 +0300

    x
    x

--- sei_help.sh
+++ sei_help.sh
@@ -18,7 +18,7 @@ echo -e "\e[0m\e[36mwithdraw all rewards:\t\t\t \e[0m\e[32mseid tx distribution 
 echo -e "\e[0m\e[36mWithdraw rewards with commision:\t \e[0m\e[32mseid tx distribution withdraw-rewards \033[33;4m<ValidatorAddress>\e[0m\e[32m --from=\033[33;4m<wallet_name>\e[0m\e[32m --commission --chain-id=\033[33;4m<chain_id>\e[0m"
 echo -e "\e[0m\e[36mEdit validator:\t\t\t\t \e[0m\e[32mseid tx staking edit-validator --moniker=\033[33;4m<moniker_name>\e[0m\e[32m --website=\033[33;4m<your_website>\e[0m\e[32m --details=\033[33;4m<your_validator_description>\e[0m\e[32m --chain-id=\033[33;4m<chain_id>\e[0m\e[32m --from=\033[33;4m<wallet_name>\e[0m"
 echo -e "\e[0m\e[36mUnjail validator:\t\t\t \e[0m\e[32mseid tx slashing unjail --broadcast-mode=block --from=\033[33;4m<wallet_name>\e[0m\e[32m --chain-id=\033[33;4m<chain_id>\e[0m\e[32m --gas=auto\e[0m"
-echo -e "\e[0m\e[36mVote proposals:\t\t\t\e[0m\e[32mseid tx gov vote \033[33;4mX\e[0m\e[32m yes --from=\033[33;4m<wallet_name>\e[0m\e[32m --chain-id=\033[33;4m<chain_id>\n\t\t\t\t\t\t\t  ┕━\033[33;4mX\e[0m\e[32m proposal id, check here \033[33;4mhttps://sei.explorers.guru/proposals\e[0m"
+echo -e "\e[0m\e[36mVote proposals:\t\t\t\t \e[0m\e[32mseid tx gov vote \033[33;4mX\e[0m\e[32m yes --from=\033[33;4m<wallet_name>\e[0m\e[32m --chain-id=\033[33;4m<chain_id>\n\t\t\t\t\t\t\t  ┕━\033[33;4mX\e[0m\e[32m proposal id, check here \033[33;4mhttps://sei.explorers.guru/proposals\e[0m"
 
 
 
