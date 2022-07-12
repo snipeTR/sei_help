7541b633756a1eeafafb5ddd683909a1082bb906
snipeTR <31975916+snipeTR@users.noreply.github.com>
2022-07-12 14:59:49 +0300

    Update sei_help.sh

--- sei_help.sh
+++ sei_help.sh
@@ -1,6 +1,3 @@
-sudo wget https://raw.githubusercontent.com/snipeTR/sei_help/main/sei_help.sh> /dev/null 2>&1
-chmod +x ./sei_help.sh> /dev/null 2>&1
-sudo mv ./sei_help.sh /usr/local/bin/helpsei> /dev/null 2>&1
 echo -e '\e[0m\e[31m=============== \e[0m\e[32mCommands that will make your job easier. Please note..\e[0m\e[31m===================\e[0m'
 echo -e '\e[0m\e[36mcheck LOGS:\t\t\t\t \e[0m\e[32mjournalctl -ujournalctl -u seid -f -o cat\e[0m'
 echo -e '\e[0m\e[36msend sei token to other address:\t \e[0m\e[32mseid tx bank send \033[33;4m<WalletName> <ToAddress> <amount>\e[0m\e[32m000000usei -y\e[0m'
@@ -19,9 +16,9 @@ echo -e "\e[0m\e[36mstaking delegate:\t\t\t \e[0m\e[32mseid tx staking delegate 
 echo -e "\e[0m\e[36mstaking redelegate:\t\t\t \e[0m\e[32mseid tx staking redelegate \033[33;4m<srcValidatorAddress> <destValidatorAddress>\e[0m\e[32m \033[33;4m<amount>\e[0m\e[32m000000usei --from=\033[33;4m<wallet_name>\e[0m\e[32m --chain-id=\033[33;4m<chain_id>\e[0m\e[32m --gas=auto\e[0m"
 echo -e "\e[0m\e[36mwithdraw all rewards:\t\t\t \e[0m\e[32mseid tx distribution withdraw-all-rewards --from=\033[33;4m<wallet_name>\e[0m\e[32m --chain-id=\033[33;4m<chain_id>\e[0m\e[32m --gas=auto\e[0m"
 echo -e "\e[0m\e[36mWithdraw rewards with commision:\t \e[0m\e[32mseid tx distribution withdraw-rewards \033[33;4m<ValidatorAddress>\e[0m\e[32m --from=\033[33;4m<wallet_name>\e[0m\e[32m --commission --chain-id=\033[33;4m<chain_id>\e[0m"
-echo -e "\e[0m\e[36mEdit validator:\t\t \e[0m\e[32mseid tx staking edit-validator --moniker=\033[33;4m<moniker_name>\e[0m\e[32m --website=\033[33;4m<your_website>\e[0m\e[32m --details=\033[33;4m<your_validator_description>\e[0m\e[32m --chain-id=\033[33;4m<chain_id>\e[0m\e[32m --from=\033[33;4m<wallet_name>\e[0m"
+echo -e "\e[0m\e[36mEdit validator:\t\t\t\t \e[0m\e[32mseid tx staking edit-validator --moniker=\033[33;4m<moniker_name>\e[0m\e[32m --website=\033[33;4m<your_website>\e[0m\e[32m --details=\033[33;4m<your_validator_description>\e[0m\e[32m --chain-id=\033[33;4m<chain_id>\e[0m\e[32m --from=\033[33;4m<wallet_name>\e[0m"
 echo -e "\e[0m\e[36mUnjail validator:\t\t\t \e[0m\e[32mseid tx slashing unjail --broadcast-mode=block --from=\033[33;4m<wallet_name>\e[0m\e[32m --chain-id=\033[33;4m<chain_id>\e[0m\e[32m --gas=auto\e[0m"
-echo -e "\e[0m\e[36mVote proposals:\t\t \e[0m\e[32mseid tx gov vote \033[33;4mX\e[0m\e[32m yes --from=\033[33;4m<wallet_name>\e[0m\e[32m --chain-id=\033[33;4m<chain_id>\n\t\t\t\t\t\t\t  ┕━\033[33;4mX\e[0m\e[32m proposal id, check here \033[33;4mhttps://sei.explorers.guru/proposals\e[0m"
+echo -e "\e[0m\e[36mVote proposals:\t\t\t\t  \e[0m\e[32mseid tx gov vote \033[33;4mX\e[0m\e[32m yes --from=\033[33;4m<wallet_name>\e[0m\e[32m --chain-id=\033[33;4m<chain_id>\n\t\t\t\t\t\t\t  ┕━\033[33;4mX\e[0m\e[32m proposal id, check here \033[33;4mhttps://sei.explorers.guru/proposals\e[0m"
 
 
 
